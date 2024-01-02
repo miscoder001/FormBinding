7a3322c6b7a15e9d3b3162126aa2ae0c4f1419c8
miscoder001 <38873656+miscoder001@users.noreply.github.com>
2024-01-02 16:57:50 +0800

    jquery 版本

---
+++ FormBinding/src/main/java/tw/mymis/formbinding/controller/MainController.java
@@ -0,0 +1,14 @@
+package tw.mymis.formbinding.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("msg", "這是 Controller傳給首頁的資料");
+        return "index";
+    }
+}
